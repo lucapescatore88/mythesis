2b5e4354e80276cbcbbee3f730e209a4e3c507ff
Luca Pescatore <luca.pescatore@cern.ch>
2016-04-09 11:44:41 +0200

    Nigel correctons ch 3 almost done

--- doWordCount.sh
+++ doWordCount.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-wc=`perl wordCount/texcount.pl -sum -merge -q -0 thesis.tex`
+#wc=`perl wordCount/texcount.pl -sum -merge -q -0 thesis.tex`
+wc=`texcount thesis.tex -sum -merge -0`
+
 echo $wc | grep '[0-9]*'
 wc=`$wc | tr -d '(error:38)' '          '`
 sed -i 's|\(.*\)||' log
 wc=`cat log`
 
-wc=`python doWordCount.py`
+#wc=`python doWordCount.py`
+#wc=`texcount thesis.tex -sum -merge -0`
+
 echo "Number of words = "$wc
 t=`date +%s`
 printf "%u %u\n" "${t}" "${wc}" >> wordCountHistory
